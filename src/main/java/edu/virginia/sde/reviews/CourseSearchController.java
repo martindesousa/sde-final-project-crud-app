5640c084aab44af28a4110083c1ee76b42560cb5
ridgeredd <113300015+ridgeredd@users.noreply.github.com>
2023-11-30 18:54:42 -0500

    worked some more

--- src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -1,13 +1,21 @@
 package edu.virginia.sde.reviews;
 
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class CourseSearchController {
+
+
+    @FXML
+    private Label searchErrorLabel;
 
     public void handleAddCourse(){
 
     }
 
     public void handleSearch(){
-
+        searchErrorLabel.setText("Course Does Not Exist");
     }
 
     public void handleLogOut(){
