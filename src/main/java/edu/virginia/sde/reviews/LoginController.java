14da7934205b661580961eeeb92cb61687f452e3
martindesousa <martin@20165.org>
2023-11-30 17:39:03 -0500

    added exit button

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -1,4 +1,23 @@
 package edu.virginia.sde.reviews;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class LoginController {
+    @FXML
+    private Label messageLabel;
+    @FXML
+    private Button exitButton;
+
+    public void handleExit() {
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+        stage.close();
+
+    }
+    public void handleUserCreated() {
+        messageLabel.setText("User created successfully.");
+    }
+
 }
