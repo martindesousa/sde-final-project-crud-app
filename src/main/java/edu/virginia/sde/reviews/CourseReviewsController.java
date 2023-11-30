4bf6410b769164137c92338ac2538bfedd4dd453
ridgeredd <113300015+ridgeredd@users.noreply.github.com>
2023-11-30 18:45:49 -0500

    workedsome more

--- src/main/java/edu/virginia/sde/reviews/CourseReviewsController.java
+++
@@ -1,4 +0,0 @@
-package edu.virginia.sde.reviews;
-
-public class CourseReviewsController {
-}
---
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -0,0 +1,20 @@
+package edu.virginia.sde.reviews;
+
+public class CourseSearchController {
+
+    public void handleAddCourse(){
+
+    }
+
+    public void handleSearch(){
+
+    }
+
+    public void handleLogOut(){
+
+    }
+
+    public void handleMyReviews(){
+
+    }
+}
