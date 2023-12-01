0c46b988f0b7bcec7c24cc7ba5c07a04bc0a8ea5
jadbeidas <jadbeidas429@gmail.com>
2023-11-30 22:22:49 -0500

    Added some methods to the database service and created a conflictswith method in the Course class

--- src/main/java/edu/virginia/sde/reviews/DatabaseService.java
+++ src/main/java/edu/virginia/sde/reviews/DatabaseService.java
@@ -2,6 +2,7 @@ package edu.virginia.sde.reviews;
 
 import jakarta.persistence.PersistenceException;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import java.util.logging.Level;
 
@@ -20,6 +21,34 @@ public class DatabaseService {
             //ERROR OCCURRED WHEN TRYING TO ADD USER
             return;
         }
+    }
+
+    public void addCourse(Course course)
+    {
+        String hql = "FROM Course c WHERE c.subjectMnemonic = :"+course.getSubjectMnemonic()+
+                " AND c.courseNumber = :"+course.getCourseNumber();
+        Query<Course> query = session.createQuery(hql, Course.class);
+        for(Course c : query.list())
+        {
+            if(c.conflictsWith(course))
+            {
+                return;
+            }
+        }
+        session.persist(course);
+        session.getTransaction().commit();
+    }
+
+    public void removeCourse(Course course)
+    {
+//        String hql = "FROM Review r WHERE r.course = :"+course;
+//        Query<Review> query = session.createQuery(hql, Review.class);
+//        for(Review r : query.list())
+//        {
+//            session.remove(r);
+//        }
+        session.remove(course);
+        session.getTransaction().commit();
     }
 
 
