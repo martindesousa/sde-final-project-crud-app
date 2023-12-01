5c3b974a345f608bb5bb8e48751d7bbf61ece60e
ridgeredd <113300015+ridgeredd@users.noreply.github.com>
2023-11-30 20:05:09 -0500

    worked some more

--- src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -3,22 +3,49 @@ package edu.virginia.sde.reviews;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class CourseSearchController {
 
 
     @FXML
-    private Label searchErrorLabel;
+    private Label addCourseErrorLabel;
+
+    @FXML
+    private TextField newCourseSubject;
+
+    @FXML
+    private TextField newCourseNumber;
+
+    @FXML
+    private TextField newCourseTitle;
 
     public void handleAddCourse(){
+
+        NewCourseValidator validator = new NewCourseValidator();
+
+        if(!validator.ValidateSubject(newCourseSubject.getText())) {
+            addCourseErrorLabel.setText("Course Subject Must Be 2-4 Letters");
+            return;
+        }else if(!validator.ValidateNumber(newCourseNumber.getText())) {
+            addCourseErrorLabel.setText("Course Number Must Be 4 Numbers");
+            return;
+        }else if(!validator.ValidateTitle(newCourseTitle.getText())) {
+            addCourseErrorLabel.setText("Course Title Must Be Between 1 and 50 Characters Long");
+            return;
+        }
+        addCourseErrorLabel.setText("");
+        //add to database
+        //
 
     }
 
     public void handleSearch(){
-        searchErrorLabel.setText("Course Does Not Exist");
+
     }
 
     public void handleLogOut(){
+
 
     }
 
---
+++ src/main/java/edu/virginia/sde/reviews/NewCourseValidator.java
@@ -0,0 +1,51 @@
+package edu.virginia.sde.reviews;
+
+import javafx.scene.control.Label;
+
+public class NewCourseValidator {
+
+    public boolean ValidateTitle(String text){
+        if(text == null){return false;}
+        if(text.length() < 1){return false;}
+        if(text.length() >50){return false;}
+        return true;
+    }
+
+    public boolean ValidateNumber(String number){
+        if(number == null){return false;}
+        if(number.length()!=4){return false;}
+        char[] numberArray = number.toCharArray();
+        for(int i=0;i<4;i++){
+            switch (numberArray[i]){
+                case '0','1','2','3','4','5','6','7','8','9':
+                    break;
+
+                default:
+                    return false;
+            }
+        }
+        return true;
+
+    }
+
+    public boolean ValidateSubject(String subject){
+
+        if(subject == null){return false;}
+        if(subject.length()<2){return false;}
+        if(subject.length()>4){return false;}
+        subject = subject.toUpperCase();
+        char[] numberArray = subject.toCharArray();
+        for(int i=0;i<subject.length();i++){
+            switch (numberArray[i]){
+                case 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z':
+                    break;
+
+                default:
+                    return false;
+            }
+        }
+        return true;
+
+    }
+
+}
