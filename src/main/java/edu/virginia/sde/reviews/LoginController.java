8f280c04d35534aa63071dff2a434bc99f51ed13
ridgeredd <113300015+ridgeredd@users.noreply.github.com>
2023-11-30 20:35:13 -0500

    worked some more

--- src/main/java/edu/virginia/sde/reviews/LoginController.java
+++ src/main/java/edu/virginia/sde/reviews/LoginController.java
@@ -1,4 +1,37 @@
 package edu.virginia.sde.reviews;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+
+    private Stage stage;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void handleLogin(){
+        LoginValidator validator = new LoginValidator();
+
+    }
+
+    private void successfulLogIn(){
+        try {
+            //This was based on the JavaFXML HotDog votes "handleGraph" method
+            var fxmlLoader = new FXMLLoader(LoginController.class.getResource("course-search.fxml"));
+            var newScene = new Scene(fxmlLoader.load());
+            var controller = (CourseSearchController) fxmlLoader.getController();
+            controller.setStage(stage);
+            stage.setScene(newScene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
