36245eb387e354b77f6341eeb5c01d0bd33fa820
martindesousa <martin@20165.org>
2023-12-01 22:58:13 -0500

    Updated LoginValidator methods

--- src/main/java/edu/virginia/sde/reviews/LoginValidator.java
+++ src/main/java/edu/virginia/sde/reviews/LoginValidator.java
@@ -1,13 +1,22 @@
 package edu.virginia.sde.reviews;
 
+import org.hibernate.Session;
+
 public class LoginValidator {
 
-    public static boolean validateUsername(String username){
-        return true;
+    public static boolean validateUsername(String username, DatabaseService service){
+        User user = new User();
+        user.setUsername(username);
+        boolean usernameExists = service.usernameExists(user);
+        return usernameExists;
     }
 
-    public static boolean validatePassword(String username, String password){
-        return true;
+    public static boolean validatePassword(String username, String password, DatabaseService service){
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        boolean passwordCorrect = service.passwordCorrect(user);
+        return passwordCorrect;
     }
 
 
